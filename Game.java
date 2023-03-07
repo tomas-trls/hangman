2b9ba5cf6ef2699b2c094ef01775660ea12004d6
Tomas Truyols <tomast25@hotmail.com>
2023-03-07 16:01:44 +0000

    feat: added a system to check the user input in a loop, if answered correct will display a victory string, othewise will explain to the player what is going on

--- Game.java
+++ Game.java
@@ -18,10 +18,41 @@ public class Game {
             System.out.println("Selecting a random word...");
             System.out.println("... ... ... ... ... .... ... ...");
             System.out.println("...Starting the game...");
-            System.out.printf("Good Luck %s", player.getName());
             System.out.println(randomWord);
+            System.out.println(player.getLives());
+
+            String[] letters = breakTheWord();
+
+            while(player.getLives() != 0){
+                System.out.println(player.getLives());
+                System.out.println("Please add a letter:");
+                String input = scanner.nextLine();
+                if(input.length() == letters.length) {
+                    if(input.toUpperCase().equals(randomWord)){
+                        System.out.printf("You win, the word was %s", randomWord);
+                        break;
+                    } else {
+                        player.setLives(player.getLives() - 1);
+                    }
+                } else if (input.length() > 1 || input.equals("")) {
+                    System.out.println("Please enter only one letter or guess a word with the exact same number of letters.");
+                } else {
+                    System.out.printf("You tried %s \n", input);
+                    player.setLives(player.getLives() - 1);
+                }
+                if(player.getLives() == 0) {
+                    System.out.printf("\n You loose, the word was %s", randomWord );
+                }
+            }
+
+
         }
     }
+
+    String[] breakTheWord(){
+        return randomWord.split("");
+    }
+
 
 
     String stopGame(){
