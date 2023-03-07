77918f2da65caa65ff9d3c3cb2695fb737a6b5fe
Tomas Truyols <tomast25@hotmail.com>
2023-03-07 17:16:23 +0000

    feat: The player is now able to enter an input and see it display live if its correct, or be told that it is wrong

--- Game.java
+++ Game.java
@@ -18,18 +18,28 @@ public class Game {
             System.out.println("Selecting a random word...");
             System.out.println("... ... ... ... ... .... ... ...");
             System.out.println("...Starting the game...");
-            System.out.println(randomWord);
-            System.out.println(player.getLives());
+            //System.out.println(randomWord);
 
-            String[] letters = breakTheWord();
+           String[] guessArr = new String[randomWord.length()];
+
+            for (int i = 0; i < guessArr.length; i++) {
+                guessArr[i] = "_";
+            }
+
+            String[] wordCharArr = randomWord.split("");
+
 
             while(player.getLives() != 0){
-                System.out.println(player.getLives());
+                System.out.println("Remaining ❤️ lives: " + player.getLives());
+                String guessString = String.join("", guessArr);
+                System.out.println(guessString);
                 System.out.println("Please add a letter:");
                 String input = scanner.nextLine();
-                if(input.length() == letters.length) {
+
+
+                if(input.length() == randomWord.length()) {
                     if(input.toUpperCase().equals(randomWord)){
-                        System.out.printf("You win, the word was %s", randomWord);
+                        System.out.printf("🎉 You win 🌟, the word was %s", randomWord);
                         break;
                     } else {
                         player.setLives(player.getLives() - 1);
@@ -37,8 +47,47 @@ public class Game {
                 } else if (input.length() > 1 || input.equals("")) {
                     System.out.println("Please enter only one letter or guess a word with the exact same number of letters.");
                 } else {
-                    System.out.printf("You tried %s \n", input);
-                    player.setLives(player.getLives() - 1);
+                    if(randomWord.contains(input)) {
+                        int index = 0;
+                        for (int i = 0; i < wordCharArr.length; i++) {
+                            if(input.contains(wordCharArr[i])){
+                                index = i;
+                                break;
+                            }
+                        }
+                        guessArr[index] = input;
+                        System.out.println("✅ Correct!");
+                    } else {
+                        switch (player.getLives()) {
+                            case 8:
+                                System.out.printf("❌ Wrong Letter! You tried %s \n", input);
+                                break;
+                            case 7:
+                                System.out.printf("❌ Wrong Letter! 😕 You tried %s \n", input);
+                                break;
+                            case 6:
+                                System.out.printf("❌ Wrong Letter! 😡 You tried %s \n", input);
+                                break;
+                            case 5:
+                                System.out.printf("❌ Wrong Letter! 😓 You tried %s \n", input);
+                                break;
+                            case 4:
+                                System.out.printf("❌ Wrong Letter! 😨 You tried %s \n", input);
+                                break;
+                            case 3:
+                                System.out.printf("❌ Wrong Letter! 😰 You tried %s \n", input);
+                                break;
+                            case 2:
+                                System.out.printf("❌ Wrong Letter! 😱 You tried %s \n", input);
+                                break;
+                            case 1:
+                                System.out.printf("❌ Wrong Letter! 🥶 You tried %s \n", input);
+                                break;
+                        }
+                        player.setLives(player.getLives() - 1);
+                    }
+
+
                 }
                 if(player.getLives() == 0) {
                     System.out.printf("\n You loose, the word was %s", randomWord );
@@ -48,11 +97,6 @@ public class Game {
 
         }
     }
-
-    String[] breakTheWord(){
-        return randomWord.split("");
-    }
-
 
 
     String stopGame(){
