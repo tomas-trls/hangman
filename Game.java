f504ed4e93b56b4f63dc51094116b00ffdfbc923
Tomas Truyols <tomast25@hotmail.com>
2023-03-08 14:29:43 +0000

    feat: finished game, fixed error with multiple letters

--- Game.java
+++ Game.java
@@ -2,25 +2,26 @@ import java.util.Scanner;
 
 public class Game {
 
-    String[] wordsArr= {"france", "elephant", "giraffe", "Nology","Spain", "Marseille", "Javascript"};
+    String[] wordsArr = {"france", "elephant", "giraffe", "Nology", "Spain", "Marseille", "Javascript"};
     WordLibrary words = new WordLibrary(wordsArr);
     String randomWord = words.getRandomWord();
+
     void launchGame() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the HangMan Game! \n Please select an option: \n 1: Play | 2: Quit ");
         String option = scanner.nextLine();
-        if(option.equals("2")){
+        if (option.equals("2")) {
             System.out.println(stopGame());
         } else {
             System.out.println("Please enter your name:");
             String name = scanner.nextLine();
-            Player player =  new Player(name);
+            Player player = new Player(name);
             System.out.println("Selecting a random word...");
             System.out.println("... ... ... ... ... .... ... ...");
             System.out.println("...Starting the game...");
             //System.out.println(randomWord);
 
-           String[] guessArr = new String[randomWord.length()];
+            String[] guessArr = new String[randomWord.length()];
 
             for (int i = 0; i < guessArr.length; i++) {
                 guessArr[i] = "_";
@@ -29,17 +30,16 @@ public class Game {
             String[] wordCharArr = randomWord.split("");
 
 
-            while(player.getLives() != 0){
+            while (player.getLives() != 0) {
                 System.out.println("Remaining ❤️ lives: " + player.getLives());
                 String guessString = String.join("", guessArr);
                 System.out.println(guessString);
                 System.out.println("Please add a letter:");
                 String input = scanner.nextLine();
 
-
-                if(input.length() == randomWord.length()) {
-                    if(input.toUpperCase().equals(randomWord)){
-                        System.out.printf("🎉 You win 🌟, the word was %s", randomWord);
+                if (input.length() == randomWord.length()) {
+                    if (input.toUpperCase().equals(randomWord)) {
+                        System.out.printf("🎉 You win 🌟 %s!, you had %s ❤ left, the word was %s",player.getName(), player.getLives(), randomWord);
                         break;
                     } else {
                         player.setLives(player.getLives() - 1);
@@ -47,41 +47,97 @@ public class Game {
                 } else if (input.length() > 1 || input.equals("")) {
                     System.out.println("Please enter only one letter or guess a word with the exact same number of letters.");
                 } else {
-                    if(randomWord.contains(input)) {
+                    if (randomWord.contains(input.toUpperCase())) {
                         int index = 0;
                         for (int i = 0; i < wordCharArr.length; i++) {
-                            if(input.contains(wordCharArr[i])){
+                            if (input.toUpperCase().contains(wordCharArr[i])) {
                                 index = i;
-                                break;
+                                guessArr[index] = input.toUpperCase();
                             }
                         }
-                        guessArr[index] = input;
+
                         System.out.println("✅ Correct!");
                     } else {
                         switch (player.getLives()) {
                             case 8:
                                 System.out.printf("❌ Wrong Letter! You tried %s \n", input);
+                                System.out.println("_______");
                                 break;
                             case 7:
                                 System.out.printf("❌ Wrong Letter! 😕 You tried %s \n", input);
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                             case 6:
                                 System.out.printf("❌ Wrong Letter! 😡 You tried %s \n", input);
+                                System.out.println("   _______");
+                                System.out.println("   |/  ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                             case 5:
                                 System.out.printf("❌ Wrong Letter! 😓 You tried %s \n", input);
+                                System.out.println("   _______");
+                                System.out.println("   |/    |");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                             case 4:
                                 System.out.printf("❌ Wrong Letter! 😨 You tried %s \n", input);
+                                System.out.println("   _______");
+                                System.out.println("   |/     |");
+                                System.out.println("   |     😣");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                             case 3:
                                 System.out.printf("❌ Wrong Letter! 😰 You tried %s \n", input);
+                                System.out.println("   _______");
+                                System.out.println("   |/     |");
+                                System.out.println("   |     😣");
+                                System.out.println("   |    💪|🤳");
+                                System.out.println("   |      ");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                             case 2:
                                 System.out.printf("❌ Wrong Letter! 😱 You tried %s \n", input);
+                                System.out.println("   _______");
+                                System.out.println("   |/     |");
+                                System.out.println("   |     😣");
+                                System.out.println("   |    💪|🤳");
+                                System.out.println("   |      |");
+                                System.out.println("   |   ");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                             case 1:
                                 System.out.printf("❌ Wrong Letter! 🥶 You tried %s \n", input);
+                                System.out.println("   _______");
+                                System.out.println("   |/     |");
+                                System.out.println("   |     😣");
+                                System.out.println("   |    💪|🤳");
+                                System.out.println("   |      |");
+                                System.out.println("   |    🦶\\");
+                                System.out.println("   |   ");
+                                System.out.println("___|___");
                                 break;
                         }
                         player.setLives(player.getLives() - 1);
@@ -89,8 +145,8 @@ public class Game {
 
 
                 }
-                if(player.getLives() == 0) {
-                    System.out.printf("\n You loose, the word was %s", randomWord );
+                if (player.getLives() == 0) {
+                    System.out.printf("\n You loose %s 😢, the word was %s",player.getName(), randomWord);
                 }
             }
 
@@ -99,10 +155,9 @@ public class Game {
     }
 
 
-    String stopGame(){
+    String stopGame() {
         return "You have quit the game.";
     }
-
 
 
 }
